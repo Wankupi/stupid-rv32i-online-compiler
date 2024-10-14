26ba47418dffaa973e6ea56038673e1c019f0705
Wankupi <2893353848@qq.com>
2024-10-14 16:28:12 +0800

    feat: change build method

---
+++ compiler/rom.s
@@ -0,0 +1,12 @@
+    .extern  main
+
+    .section .text._start
+    .global  _start
+_start:
+    li       sp, 0x00020000
+    jal      main
+    li       a0, 0xff
+.L0:
+    lui      a3, 0x30
+    sb       a0, 4(a3)
+    j        .L0
